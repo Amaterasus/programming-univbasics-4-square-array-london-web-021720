b75ce466d963b58bc75c836bad28b57344c4883d
Skye Geraghty <skyetay03@googlemail.com>
2020-01-24 13:15:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
   
   while array[counter] do
-    output << array[counter] ** 2
+    output.push(array[counter] ** 2)
     counter += 1
   end
   return output
